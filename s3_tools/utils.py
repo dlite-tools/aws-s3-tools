e9366a606b373cee7688513633861a229911b988
Daniel Ferrari <ferrari.dg@gmail.com>
2021-04-20 19:03:51 +0100

    Function to create the progress object - it will test if the rich package is installed

--- s3_tools/utils.py
+++ s3_tools/utils.py
@@ -21,3 +21,46 @@ def _get_future_output(future: futures.Future) -> Any:
         return future.result()
     except Exception as e:
         return repr(e)
+
+
+def _create_progress_bar(description: str, length: int):
+    """Create a console progress bar using 'rich' package.
+
+    Parameters
+    ----------
+    description : str
+        Progress bar description.
+    length : int
+        Progress bar length.
+
+    Returns
+    -------
+    Tuple[Optional[Progress], Optional[int]]
+        The progress bar object and the task ID associated.
+        (Need to install extra [progress] to be used)
+    """
+    try:
+        from rich.progress import (
+            BarColumn,
+            Progress,
+            TextColumn
+        )
+
+        progress = Progress(
+            TextColumn("[bold blue]{task.description}", justify="right"),
+            BarColumn(bar_width=None),
+            TextColumn("[bold blue][{task.completed}/{task.total}]"),
+        )
+
+        task_id = progress.add_task(
+            description=description,
+            total=length,
+            start=False
+        )
+
+    except ImportError:
+        print("Please install rich package. "
+              "See the docs at https://rich.readthedocs.io/en/stable/introduction.html for more information.")
+        raise
+
+    return progress, task_id
