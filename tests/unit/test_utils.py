4256bfcf877f5b12d9d7bf3e46399a7c5cc350eb
Daniel Ferrari <ferrari.dg@gmail.com>
2021-04-20 19:05:03 +0100

    Add unit tests to create-progress-bar

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,7 +1,25 @@
 """Unit tests for utils.py"""
+import builtins
 from concurrent import futures
 
-from s3_tools.utils import _get_future_output
+import pytest
+
+from s3_tools.utils import (
+    _create_progress_bar,
+    _get_future_output
+)
+
+
+@pytest.fixture
+def hide_available_pkg(monkeypatch):
+    import_orig = builtins.__import__
+
+    def mocked_import(name, *args, **kwargs):
+        if "rich." in name:
+            raise ModuleNotFoundError()
+        return import_orig(name, *args, **kwargs)
+
+    monkeypatch.setattr(builtins, "__import__", mocked_import)
 
 
 class TestFuture:
@@ -24,3 +42,21 @@ class TestFuture:
 
         assert len(responses) == 5
         assert sorted(responses)[0][1] == "ZeroDivisionError('division by zero')"
+
+
+class TestProgressBar:
+
+    @pytest.mark.usefixtures("hide_available_pkg")
+    def test_if_no_package(self):
+        with pytest.raises(ModuleNotFoundError):
+            _create_progress_bar("Test", 10)
+
+    def test_create_progress_bar(self):
+        pytest.importorskip("rich")
+
+        progress, task_id = _create_progress_bar("Test", 10)
+        print(progress, task_id)
+
+        assert len(progress.columns) == 3
+        assert task_id == 0
+        assert progress.tasks[task_id].total == 10
