9a6683d32784c7db2830fe25011e2cb4eaf93613
Carlos Alves <cmalves.426@gmail.com>
2022-03-08 20:30:23 +0000

    tests: add assert url not none

--- tests/unit/objects/test_presigned_url.py
+++ tests/unit/objects/test_presigned_url.py
@@ -28,6 +28,7 @@ class TestPresignedUrl:
             response = requests.get(url)
             response.raise_for_status()
 
+            assert url is not None
             assert response.status_code == 404
 
     def test_download_object_with_presigned_url(self, s3_client):
@@ -43,6 +44,7 @@ class TestPresignedUrl:
             url = get_presigned_url(client_method='list_objects', method_parameters={'Bucket': BUCKET_NAME})
             response = requests.get(url)
 
+        assert url is not None
         assert response.status_code == 200
 
     def test_invalid_request_with_presigned_url(self, s3_client):
@@ -50,7 +52,7 @@ class TestPresignedUrl:
             url = get_presigned_url(client_method='list_objects')
             requests.get(url)
 
-    def test_post_objects_with_presigned_url(self, s3_client):
+    def test_upload_objects_with_presigned_url(self, s3_client):
         with create_bucket(s3_client, BUCKET_NAME):
             before = object_exists(BUCKET_NAME, self.key)
             response = get_presigned_upload_url(bucket=BUCKET_NAME, key=self.key)
@@ -60,10 +62,11 @@ class TestPresignedUrl:
 
             after = object_exists(BUCKET_NAME, self.key)
 
+        assert response is not None
         assert post_response.status_code == 204
         assert before is False
         assert after is True
 
-    def test_invalid_post_objects_with_presigned_url(self, s3_client):
+    def test_invalid_upload_objects_with_presigned_url(self, s3_client):
         with pytest.raises(AttributeError):
             get_presigned_upload_url(bucket=BUCKET_NAME, key=None)
