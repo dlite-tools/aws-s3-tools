d9185630c001864b14b7db6e3f43792cc5c7953d
Daniel Ferrari <ferrari.dg@gmail.com>
2022-08-10 16:55:28 +0100

    feat: Add Path to move functions

--- tests/unit/objects/test_move_objects.py
+++ tests/unit/objects/test_move_objects.py
@@ -1,17 +1,19 @@
 """Unit tests for move.py"""
+from pathlib import Path
+
 import pytest
 from botocore.exceptions import ClientError
 
 from s3_tools import (
     move_keys,
     move_object,
-    object_exists
+    object_exists,
 )
 
 from tests.unit.conftest import (
     create_bucket,
     BUCKET_NAME,
-    FILENAME
+    FILENAME,
 )
 
 
@@ -23,6 +25,9 @@ class TestMove:
 
     source_keys = [f"prefix/object_0{i}" for i in range(1, 5)]
     destination_keys = [f"new-prefix/new-object_0{i}" for i in range(1, 5)]
+
+    source_keys_path = [Path(key) for key in source_keys]
+    destination_keys_path = [Path(key) for key in destination_keys]
 
     def test_move_from_nonexisting_bucket(self, s3_client):
         try:
@@ -41,30 +46,42 @@ class TestMove:
 
         assert error == "404"
 
-    def test_move_inside_bucket(self, s3_client):
-        with create_bucket(s3_client, BUCKET_NAME, keys_paths=[(self.source_key, FILENAME)]):
-            source_before = object_exists(BUCKET_NAME, self.source_key)
-            dest_before = object_exists(BUCKET_NAME, self.destination_key)
+    @pytest.mark.parametrize("source,destination", [
+        (source_key, destination_key),
+        (source_key, Path(destination_key)),
+        (Path(source_key), destination_key),
+        (Path(source_key), Path(destination_key)),
+    ])
+    def test_move_inside_bucket(self, s3_client, source, destination):
+        with create_bucket(s3_client, BUCKET_NAME, keys_paths=[(source, FILENAME)]):
+            source_before = object_exists(BUCKET_NAME, source)
+            dest_before = object_exists(BUCKET_NAME, destination)
 
-            move_object(BUCKET_NAME, self.source_key, BUCKET_NAME, self.destination_key)
+            move_object(BUCKET_NAME, source, BUCKET_NAME, destination)
 
-            source_after = object_exists(BUCKET_NAME, self.source_key)
-            dest_after = object_exists(BUCKET_NAME, self.destination_key)
+            source_after = object_exists(BUCKET_NAME, source)
+            dest_after = object_exists(BUCKET_NAME, destination)
 
         assert source_before is True and dest_before is False
         assert source_after is False and dest_after is True
 
-    def test_move_between_bucket(self, s3_client):
-        with create_bucket(s3_client, BUCKET_NAME, keys_paths=[(self.source_key, FILENAME)]), \
+    @pytest.mark.parametrize("source,destination", [
+        (source_key, destination_key),
+        (source_key, Path(destination_key)),
+        (Path(source_key), destination_key),
+        (Path(source_key), Path(destination_key)),
+    ])
+    def test_move_between_bucket(self, s3_client, source, destination):
+        with create_bucket(s3_client, BUCKET_NAME, keys_paths=[(source, FILENAME)]), \
                 create_bucket(s3_client, self.destination_bucket):
 
-            source_before = object_exists(BUCKET_NAME, self.source_key)
-            dest_before = object_exists(self.destination_bucket, self.destination_key)
+            source_before = object_exists(BUCKET_NAME, source)
+            dest_before = object_exists(self.destination_bucket, destination)
 
-            move_object(BUCKET_NAME, self.source_key, self.destination_bucket, self.destination_key)
+            move_object(BUCKET_NAME, source, self.destination_bucket, destination)
 
-            source_after = object_exists(BUCKET_NAME, self.source_key)
-            dest_after = object_exists(self.destination_bucket, self.destination_key)
+            source_after = object_exists(BUCKET_NAME, source)
+            dest_after = object_exists(self.destination_bucket, destination)
 
         assert source_before is True and dest_before is False
         assert source_after is False and dest_after is True
@@ -84,18 +101,24 @@ class TestMove:
 
     def test_move_list_different_length(self, s3_client):
         with create_bucket(s3_client, BUCKET_NAME), pytest.raises(IndexError):
-            move_keys(BUCKET_NAME, self.source_keys, self.destination_bucket, [])
+            move_keys(BUCKET_NAME, [self.source_key], self.destination_bucket, [])
 
-    def test_move_list_inside_bucket(self, s3_client):
-        keys_paths = [(key, FILENAME) for key in self.source_keys]
+    @pytest.mark.parametrize("source,destination", [
+        (source_keys, destination_keys),
+        (source_keys, destination_keys_path),
+        (source_keys_path, destination_keys),
+        (source_keys_path, destination_keys_path),
+    ])
+    def test_move_list_inside_bucket(self, s3_client, source, destination):
+        keys_paths = [(key, FILENAME) for key in source]
         with create_bucket(s3_client, BUCKET_NAME, keys_paths=keys_paths):
-            source_before = [object_exists(BUCKET_NAME, key) for key in self.source_keys]
-            dest_before = [object_exists(BUCKET_NAME, key) for key in self.destination_keys]
+            source_before = [object_exists(BUCKET_NAME, key) for key in source]
+            dest_before = [object_exists(BUCKET_NAME, key) for key in destination]
 
-            move_keys(BUCKET_NAME, self.source_keys, BUCKET_NAME, self.destination_keys)
+            move_keys(BUCKET_NAME, source, BUCKET_NAME, destination)
 
-            source_after = [object_exists(BUCKET_NAME, key) for key in self.source_keys]
-            dest_after = [object_exists(BUCKET_NAME, key) for key in self.destination_keys]
+            source_after = [object_exists(BUCKET_NAME, key) for key in source]
+            dest_after = [object_exists(BUCKET_NAME, key) for key in destination]
 
             assert all(source_before) is True and all(dest_before) is False
             assert all(source_after) is False and all(dest_after) is True
